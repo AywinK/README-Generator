c158f90b27ee8cbfd3f94950831f97899fdc8b75
AywinK <115905373+AywinK@users.noreply.github.com>
2023-01-21 14:19:02 +0000

    wip

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ inquirer.prompt([{
 }, {
     type: "input",
     name: "description",
-    message: "Provide a short description of the application.",
+    message: "Provide a short description of the project.",
     validate: (userInput) => (userInput ? true : "You must provide a description.")
 }, {
     type: "input",
@@ -25,12 +25,36 @@ inquirer.prompt([{
     name: "license",
     message: "Select a license.",
     choices: ["Apache License 2.0", "GNU GPLv3", "MIT", "No License"]
-},]).then(userData => {
-    console.log(`Your Project Title is: ${userData.title}`);
-    console.log(`Your Description is: ${userData.description}`);
-    console.log(`Your Installation Instructions are: ${(userData.installation ? userData.installation : "N/A")}`);
-    console.log(`Your Usage Information is: ${userData.usage}`);
-    console.log(`Your License is: ${userData.license}`);
+}, {
+    type: "input",
+    name: "contributing",
+    message: "Provide information on how others can contribute to development.",
+    validate: (userInput) => (userInput ? true : "You must provide information regarding project participation.")
+}, {
+    type: "input",
+    name: "tests",
+    message: "Provide instructions to run tests for your application."
+}, {
+    type: "input",
+    name: "githubUsername",
+    message: "Provide your github username.",
+    validate: (userInput) => (userInput ? true : "Provide your GitHub username so others can reach you.")
+}, {
+    type: "input",
+    name: "email",
+    message: "Provide your email address.",
+    validate: (userInput) => (userInput ? true : "Provide your email address so others can contact you.")
+}]).then(readmeContent => {
+    console.log(`Your Project Title is: ${readmeContent.title}`);
+    console.log(`Your Description is: ${readmeContent.description}`);
+    console.log(`Your Installation Instructions are: ${(readmeContent.installation ? readmeContent.installation : "N/A")}`);
+    console.log(`Your Usage Information is: ${readmeContent.usage}`);
+    console.log(`Your License is: ${readmeContent.license}`);
+    console.log(`Your Contributing Information is: ${readmeContent.contributing}`);
+    console.log(`Your tests Instructions are: ${(readmeContent.tests ? readmeContent.tests : "N/A")}`);
+    console.log(`Contact the developers: ${readmeContent.githubUsername} - https://github.com/${readmeContent.githubUsername}`);
+    console.log(`Contact the developers: ${readmeContent.email}`);
+
 });
 
 
---
+++ readmePlayground.md
@@ -0,0 +1,32 @@
+# Sample Project
+
+ ![License Badge](https://img.shields.io/badge/License-MIT-green?labelColor=434343)
+## Description
+---
+thdfg
+## Table of Contents
+---
+* [Description](#Description)
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [License](#License)
+* [Contributing](#Contributing)
+* [Tests](#Tests)
+* [Questions](#Questions)
+
+## Installation
+---
+## Usage
+---
+## License
+---
+## Contributing
+---
+## Tests
+---
+## Questions
+---
+
+Discover more projects - [AywinK](https://github.com/AywinK "My GitHub Profile")
+
+ Contact <opencode@microsoft.com> with any additional questions or comments.
