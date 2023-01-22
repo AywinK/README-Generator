85071b10aec5f239c77a41f612f58c13c7a8ff2a
AywinK <115905373+AywinK@users.noreply.github.com>
2023-01-22 13:09:44 +0000

    meets app reqs.

--- index.js
+++ index.js
@@ -2,61 +2,17 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const prompts = require("./lib/prompts");
+const readmeTemplate = require("./lib/readmeTemplate");
 
-console.log(prompts.createsPrompts());
+// console.log(prompts.createsPrompts());
 
 // const testArr = prompts;
 
-inquirer.prompt(prompts.createsPrompts()).then(readmeContent => {
+inquirer.prompt(prompts.createsPrompts()).then(data => {
 
-    let readmeFile = `
-# ${readmeContent.title}
+    const readmeContent = readmeTemplate.generatesReadme(data);
 
-![License Badge](https://img.shields.io/badge/License-${readmeContent.license.replace(/\s+/g, ``)}-green?labelColor=434343)
-
-## Description
-
-${readmeContent.description}
-
-## Table of Contents
-
-* [Description](#Description)
-* [Installation](#Installation)
-* [Usage](#Usage)
-* [License](#License)
-* [Contributing](#Contributing)
-* [Tests](#Tests)
-* [Questions](#Questions)
-
-## Installation
-
-${(readmeContent.installation ? readmeContent.installation : "N/A")}
-
-## Usage
-    
-${readmeContent.usage}
-
-## License
-
-This work is licensed under the following license: ${readmeContent.license}
-
-## Contributing
-    
-${readmeContent.contributing}
-    
-## Tests
-
-${(readmeContent.tests ? readmeContent.tests : "N/A")}
-    
-## Questions
-
-Discover more projects or get in touch - [${readmeContent.githubUsername}](https://github.com/${readmeContent.githubUsername} "My GitHub Profile")
-
-Contact <${readmeContent.email}> with any additional questions or comments.
-
-    `;
-
-    fs.writeFile(`${readmeContent.title}.md`, `${readmeFile}`, (err) => err ? console.error(err) : console.log("readme sucessfully generated"));
+    fs.writeFile(`${data.title}_README.md`, `${readmeContent}`, (err) => err ? console.error(err) : console.log("readme sucessfully generated"));
 
 });
 
---
+++ testing123_README.md
@@ -0,0 +1,77 @@
+
+# testing123
+
+![License Badge](https://img.shields.io/badge/License-GNU_GPLv3-green?labelColor=434343)
+
+## Description
+
+shortanddesciptbsa
+
+## Table of Contents
+
+* [Description](#Description)
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [License](#License)
+* [Contributing](#Contributing)
+* [Tests](#Tests)
+* [Questions](#Questions)
+
+## Installation
+
+run this
+
+```
+npm i
+```
+
+## Usage
+
+run app using
+
+```
+node index.js
+```
+
+![Demo](./images/demo.gif)
+
+## License
+
+This work is licensed under the following license: GNU GPLv3
+
+---
+
+
+Copyright (C) 2023 aywink 
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+## Contributing
+
+create pr
+
+## Tests
+
+run this
+
+```
+jest tests.js
+```
+
+## Questions
+
+Discover more projects or get in touch - [aywink ](https://github.com/aywink  "My GitHub Profile")
+
+Contact <ak@emai.com> with any additional questions or comments.
